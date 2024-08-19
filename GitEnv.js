4ae999acdad0cfe18e44d15fac823ba2c28c3cba
OIyiola <97890183+OIyiola@users.noreply.github.com>
2024-08-19 21:34:38 +0100

    Version3

--- GitEnv.js
+++ GitEnv.js
@@ -1,3 +1,3 @@
-console.log("GitTraining Version2 update");
-console.log("GitTraining in progress Version2");
-console.log("GitTraining in progress and update Version2");
+console.log("GitTraining Version3 update");
+console.log("GitTraining in progress Version3");
+console.log("GitTraining in progress and update Version3");
